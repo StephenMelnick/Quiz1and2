7a1ee1685a04f9b6c7af376c409aac42ca79d824
Stephen <stephenmelnick8@gmail.com>
2019-08-18 19:57:09 +0000

    Write a Note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,22 @@
 class NotesController < ApplicationController
 
-def index
-end
+  def index
+    @notes = Note.all
+  end
+
+  def new
+  @note = Note.new
+  end
+
+  def create
+    Note.create(note_params)
+    redirect_to root_path
+  end
+
+private
+
+  def note_params
+    params.require(:note).permit(:name, :date, :description)
+  end
 
 end
